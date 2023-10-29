a4b33e0081b0508d829a678fe605de4dc6253e5a
Keshavsingh0811 <keshavsingh39357@gmail.com>
2023-10-29 23:09:54 +0530

    ConversionOperation

---
+++ ConversionOperation.js
@@ -0,0 +1,10 @@
+let score = "33"
+
+console.log(typeof score);
+
+let convertNumber = Number(score)
+
+console.log(typeof convertNumber);
+
+// "33" => 33 (Convert to Number)
+// "33abc" => Nan (Not a number)
