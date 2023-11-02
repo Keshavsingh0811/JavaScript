d1952c89223909d39e217fdb603341c52183ae5e
Keshavsingh0811 <keshavsingh39357@gmail.com>
2023-11-02 17:48:29 +0530

    while and dowhile loop

---
+++ Iteration2.js
@@ -0,0 +1,51 @@
+// While Loop
+
+let index = 0
+while (index <= 10) {
+    console.log(`Value of index is ${index}`);
+    index = index + 2
+}
+
+// Value of index is 0
+// Value of index is 2
+// Value of index is 4
+// Value of index is 6
+// Value of index is 8
+// Value of index is 10
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+let myArr = ["keshav", "singh", "sunny", "singh"]
+
+let arr = 0
+
+while (arr < myArr.length) {
+    console.log(`Array name is ${myArr[arr]}`);
+    arr = arr + 1
+}
+
+// Array name is keshav
+// Array name is singh
+// Array name is sunny
+// Array name is singh
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// do while loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score<10);
+
+// Score is 1
+// Score is 2
+// Score is 3
+// Score is 4
+// Score is 5
+// Score is 6
+// Score is 7
+// Score is 8
+// Score is 9
